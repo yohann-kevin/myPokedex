7153ac90ff2a8058bff473d1c79c94e1f5566807
Perriguey <yo44prg@icloud.com>
2020-07-03 21:43:59 +0200

    fix : bug remove image

--- module/assets/closeSinglePoke.js
+++ module/assets/closeSinglePoke.js
@@ -3,8 +3,7 @@ const views = document.getElementById('id01');
 
 export function closePoke(div) {
     closeSinglePoke.addEventListener("click", function() {
+        div.children[1].remove();
         views.style.display = 'none';
-        let img = div.children
-        img[0].remove();
     })
 }
