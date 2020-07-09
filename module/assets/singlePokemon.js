a9160544bfa3b4dd17f5feda4a5b34676c4757ae
Perriguey <yo44prg@icloud.com>
2020-07-09 16:40:08 +0200

    feat : add pokeHeight and pokeWeight

--- module/assets/singlePokemon.js
+++ module/assets/singlePokemon.js
@@ -1,5 +1,7 @@
 import {backgroundColor} from './backgroundColor.js'
 let namePokemon = document.createElement('h1');
+let pokeWeight = document.createElement('h2');
+let pokeHeight = document.createElement('h2');
 
 export function singlePokemon(page,div,id,name,type,data) {
     let newImg = document.createElement('img');
@@ -7,9 +9,13 @@ export function singlePokemon(page,div,id,name,type,data) {
     page.style.display = 'block';
     namePokemon.textContent = data[1];
     namePokemon.classList.add('namePokemon');
+    pokeWeight.textContent = data[3];
+    pokeHeight.textContent = data[4];
     newImg.style.backgroundColor = backgroundColor(type);
     newImg.classList.add('imgSinglePokemon');
     div.appendChild(newImg);
     div.appendChild(namePokemon);
+    div.appendChild(pokeWeight);
+    div.appendChild(pokeHeight);
     console.log(data);
 }
