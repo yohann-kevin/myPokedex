8069c04f49b9a053bba38f762629a7cb011f209b
Perriguey <yo44prg@icloud.com>
2020-08-04 22:07:30 +0200

    feat : add page about and page contact

--- module/views.js
+++ module/views.js
@@ -1,20 +1,33 @@
 let btnPokemon = document.getElementById('testPokemon');
 let btnPokemonSmart = document.getElementById('testPokemonSmart');
+
 let btnTestBadge = document.getElementById('testBadge');
 let btnTestBadgeSmart = document.getElementById('testBadgeSmart');
 
+let btnAbout = document.getElementById('btnAbout');
+let btnAboutSmart = document.getElementById('aboutSmart');
+
+let btnContact = document.getElementById('btnContact');
+let btnContactSmart = document.getElementById('contactSmart');
+
 let pokemonViews = document.getElementById('pokeSection');
 let testBadgeViews = document.getElementById('testColor');
+let aboutViews = document.getElementById('about');
+let contactViews = document.getElementById('contact');
 
 export function views() {
     if (window.screen.width <= 760) {
         pokemonSmart();
         btnPokemonSmart.addEventListener("click", pokemonSmart);
         btnTestBadgeSmart.addEventListener("click", badgeSmart);
+        btnAboutSmart.addEventListener("click", aboutSmart);
+        btnContactSmart.addEventListener("click", contactSmart);
     } else {
         pokemon();
         btnPokemon.addEventListener("click", pokemon);
         btnTestBadge.addEventListener("click", badge);
+        btnAbout.addEventListener("click", about);
+        btnContact.addEventListener("click", contact);
     }
 }
 
@@ -32,6 +45,22 @@ function badge() {
 
 function badgeSmart() {
     openPage(testBadgeViews, btnTestBadgeSmart);
+}
+
+function about() {
+    openPage(aboutViews, btnAbout);
+}
+
+function aboutSmart() {
+    openPage(aboutViews, btnAboutSmart);
+}
+
+function contact() {
+    openPage(contactViews, btnContact);
+}
+
+function contactSmart() {
+    openPage(contactViews, btnContactSmart);
 }
 
 function openPage(pageName, link) {
