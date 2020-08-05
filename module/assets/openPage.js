2aae54751ad8f282e92c75a77f6d4ebcdc4bef08
Perriguey <yo44prg@icloud.com>
2020-08-05 21:51:42 +0200

    fix : refactor function views

---
+++ module/assets/openPage.js
@@ -0,0 +1,16 @@
+
+export function openPage(pageName, link) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.color = "";
+    }
+
+    pageName.style.display = "block";
+    link.style.color = "white";
+}
