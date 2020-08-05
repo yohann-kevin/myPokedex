37fa46b93d3aad301724793e722e98a2932a6938
Perriguey <yo44prg@icloud.com>
2020-08-05 21:35:09 +0200

    feat : custom badge in page singkePokemon

--- module/assets/singlePokemon.js
+++ module/assets/singlePokemon.js
@@ -8,7 +8,10 @@ let newId = document.createElement('h2');
 let pokeWeight = document.createElement('h2');
 let pokeHeight = document.createElement('h2');
 let type = document.createElement('h2');
+let type2 = document.createElement('h2');
 let divInfo = document.createElement('div');
+
+let whyType2 = false;
 
 export function singlePokemon(page, div, name, data) {
     let newImg = document.createElement('img');
@@ -18,18 +21,8 @@ export function singlePokemon(page, div, name, data) {
     page.style.display = 'block';
     namePokemon.textContent = data[1];
     namePokemon.classList.add('namePokemon');
-    // pokeWeight.textContent = data[3];
     pokeWeight.textContent = weightPoke(data[3]);
-    // pokeHeight.textContent = data[4];
     pokeHeight.textContent = heightPoke(data[4]);
-    
-    if (typeof data[2] === 'object') {
-        type.textContent = data[2][0] + " " + data[2][1];
-        newImg.style.backgroundColor = backgroundColor(data[2][0]);
-    } else if (typeof data[2] === 'string') {
-        type.textContent = data[2];
-        newImg.style.backgroundColor = backgroundColor(data[2]);
-    }
 
     newImg.classList.add('imgSinglePokemon');
     div.classList.add('divPoke');
@@ -40,5 +33,30 @@ export function singlePokemon(page, div, name, data) {
     div.appendChild(namePokemon);
     div.appendChild(pokeWeight);
     div.appendChild(pokeHeight);
-    div.appendChild(type);
+
+    if (typeof data[2] === 'object') {
+        type.textContent = data[2][0];
+        type2.textContent = data[2][1];
+        type.style.backgroundColor = backgroundColor(data[2][0]);
+        type2.style.backgroundColor = backgroundColor(data[2][1]);
+        newImg.style.backgroundColor = backgroundColor(data[2][0]);
+        type.style.height = "35px";
+        type.style.padding = "5px";
+        type.style.borderRadius = "5px";
+        type2.style.height = "35px";
+        type2.style.padding = "5px";
+        type2.style.borderRadius = "5px";
+        div.appendChild(type);
+        div.appendChild(type2);
+        whyType2 = true;
+    } else if (typeof data[2] === 'string') {
+        if (whyType2) type2.remove();
+        type.textContent = data[2];
+        type.style.backgroundColor = backgroundColor(data[2]);
+        type.style.height = "35px";
+        type.style.padding = "5px";
+        type.style.borderRadius = "5px";
+        newImg.style.backgroundColor = backgroundColor(data[2]);
+        div.appendChild(type);
+    }
 }
