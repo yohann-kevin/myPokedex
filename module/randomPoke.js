134a65c0abcd4be5bde76ae9562322a5e1cb293b
Perriguey <yo44prg@icloud.com>
2020-08-09 15:19:43 +0200

    feat : add singlePoke for randomPoke

--- module/randomPoke.js
+++ module/randomPoke.js
@@ -1,7 +1,12 @@
 import {pokeName} from './assets/pokeName.js'
 import {backgroundColor} from './assets/backgroundColor.js'
+import {verifyType} from './assets/verifyType.js'
+import {singlePokemon} from './assets/singlePokemon.js'
+import {closePoke} from './assets/closeSinglePoke.js'
 
 let container = document.getElementById('randomPoke');
+const pageSingle = document.getElementById('id01');
+const singleDiv = document.getElementById('container');
 
 export function randomPoke() {
     let index = randomNum();
@@ -9,8 +14,15 @@ export function randomPoke() {
         fetch("https://pokeapi.co/api/v2/pokemon/" + i).then(function (response) {
             return response.json()
         }).then(function (json) {
+            let arrayPoke = [];
             let div = document.createElement('div');
             div.classList.add('poke');
+
+            if (json.types.length === 2) {
+                arrayPoke.push(json.id,json.name,verifyType(json.types[0].type.name,json.types[1].type.name),json.weight,json.height,);
+            } else {
+                arrayPoke.push(json.id,json.name,json.types[0].type.name,json.weight,json.height,);
+            }
             
             let pName = document.createElement('a');
             pokeName(pName,json.name,'white','pokeName');
@@ -19,12 +31,18 @@ export function randomPoke() {
             pImg.alt = json.name;
 
             container.appendChild(div);
-            div.appendChild(pName);
             div.appendChild(pImg);  
+            div.appendChild(pName);
             
             div.style.backgroundColor = backgroundColor(json.types[0].type.name);
             div.style.borderRadius = '15px';
             div.style.marginBottom = "40px";
+            div.onmouseover = div.style.cursor = 'pointer';
+
+            div.addEventListener("click", function () {
+                singlePokemon(pageSingle,singleDiv,json.name,arrayPoke);
+                closePoke(singleDiv);
+            })
         });
     }
 }
