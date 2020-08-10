27ee7537a928e13230898665a3c9b71317dd3427
Perriguey <yo44prg@icloud.com>
2020-08-10 20:47:03 +0200

    fix : bug in pokeId

--- module/assets/pokeId.js
+++ module/assets/pokeId.js
@@ -2,9 +2,12 @@
 export function pokeId(id,newId) {
     if (id < 10) {
         newId.textContent = "#00" + id;
+        return newId;
     } else if (id >= 10 && id < 100) {
         newId.textContent = "#0" + id;
+        return newId;
     } else {
         newId.textContent = "#" + id;
+        return newId;
     }
 }
